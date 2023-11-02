9e163c64d2f33e32af032ae065fdc397e4c7960c
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-11-02 22:41:18 +0700

    update Homepage route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Spinner from "./components/Spinner";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Spinner />
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
           <Route path='*' element={<NotFoundPage />} />
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Footer from "../components/Footer";
+import FooterFixed from "../components/FooterFixed";
+import Header from "../components/Header";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <FooterFixed />
+      <Footer />
+    </>
+  );
+}
