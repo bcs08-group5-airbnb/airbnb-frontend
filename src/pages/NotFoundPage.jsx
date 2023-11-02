9a4a552a69651b0ed3ea0e3523edd8807d9091a4
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-11-02 22:31:56 +0700

    update not found 404 page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -6,26 +6,26 @@ export default function NotFoundPage() {
   return (
     <>
       <Header />
-      <div className='w-[95%] mx-auto grid grid-cols-1 md:grid-cols-2'>
+      <div className='w-[95%] mx-auto grid grid-cols-1 md:grid-cols-2 py-12'>
         <div className='space-y-9'>
-          <p className='text-black font-bold text-3xl'>We can’t seem to find the page you’re looking for</p>
+          <p className='text-black font-bold text-xl'>Chúng tôi dường như không thể tìm thấy trang bạn đang tìm kiếm</p>
           <div className='space-y-3'>
-            <p>Here are some helpful links instead:</p>
+            <p>Dưới đây là một số đường dẫn của website:</p>
             <ul className='space-y-3'>
               <li>
-                <a className='font-bold text-black underline cursor-pointer'>Home</a>
+                <a className='font-bold text-black underline cursor-pointer'>Trang chủ</a>
               </li>
               <li>
-                <a className='font-bold text-black underline cursor-pointer'>Traveling on Airbnb</a>
+                <a className='font-bold text-black underline cursor-pointer'>Đi du lịch trên Airbnb</a>
               </li>
               <li>
-                <a className='font-bold text-black underline cursor-pointer'>Hosting on Airbnb</a>
+                <a className='font-bold text-black underline cursor-pointer'>Làm chủ nhà trên Airbnb</a>
               </li>
               <li>
-                <a className='font-bold text-black underline cursor-pointer'>Trust & Safety</a>
+                <a className='font-bold text-black underline cursor-pointer'>Uy tín & An toàn</a>
               </li>
               <li>
-                <a className='font-bold text-black underline cursor-pointer'>Sitemap</a>
+                <a className='font-bold text-black underline cursor-pointer'>Sơ đồ trang web</a>
               </li>
             </ul>
           </div>
