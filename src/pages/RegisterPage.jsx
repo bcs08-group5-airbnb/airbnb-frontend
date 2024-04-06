05504b515ebbeeb36d506f9ed75b4c1c4ce728ea
longphanquangminh <lordaizensousuke2001@gmail.com>
2024-04-06 23:22:02 +0700

    up

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -22,7 +22,7 @@ export default function RegisterPage() {
       });
   };
   return (
-    <div className="flex flex-col min-h-screen bg-[url('https://www.travelandleisure.com/thmb/OfqaB3cuqutgOr1avyhlNPZrBh4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/bali-bamboo-house-airbnb-AIRZOOM0420-a69777600d2a427190cf1bcb187e0eb5.jpg')] bg-center bg-cover bg-no-repeat bg-fixed relative">
+    <div className="flex flex-col min-h-screen bg-[url('https://raw.githubusercontent.com/bcs08-group5-airbnb/airbnb-frontend/master/public/images/airbnb-travel-bg.jpg')] bg-center bg-cover bg-no-repeat bg-fixed relative">
       <div className='flex flex-1 justify-center items-center'>
         <div className='p-6 m-2 bg-white rounded-lg w-2/3 space-y-3'>
           <div>
